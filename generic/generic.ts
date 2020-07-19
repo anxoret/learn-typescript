f7f1e9ec6478b31c65f3f0db62d1679413489e46
anxoret <post.lux@yandex.ru>
2020-07-19 19:48:14 +0400

    Begin learning generic

---
+++ generic/generic.ts
@@ -0,0 +1,9 @@
+const arrayOfNumber: Array<number> =  [1, 1, 2, 3, 5]
+const arrayOfStrings: Array<string> =  ['Hello', 'Vladilen']
+
+function reverse<T>(array: T[]): T[] {
+    return array.reverse()
+}
+
+reverse(arrayOfNumber)
+reverse(arrayOfStrings)
